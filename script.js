0f3e10232925e00fdba5678abf2705dc997f3664
HayvenX <pg766282@gmail.com>
2023-09-19 09:17:37 +0300

    Add all

---
+++ script.js
@@ -0,0 +1,9 @@
+menubar.onclick = function myFunction() {
+    var x = document.getElementById('myTopNav');
+
+    if (x.className === "topnav") {
+        x.className += "responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
